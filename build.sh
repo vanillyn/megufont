d1a3143667de72504c77c5e264096ed9b8505268
vanillyn <108785622+vanillyn@users.noreply.github.com>
2023-05-25 16:14:41 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 mkdir -p build
+mkdir -p dist
 cd build
 wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FontPatcher.zip
 unzip FontPatcher.zip
 fontforge -script font-patcher Megufont-Regular.ttf -c
-cp "Megufont Regular Nerd Font"* ../
+cp "Megufont Regular Nerd Font"* ../dist
 cd ..
 rm -rf build
